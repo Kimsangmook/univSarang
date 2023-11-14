c1d9bbb30cab2cda89a7832f2478919b51fc2f88
Kimsangmook <uj03109@naver.com>
2023-11-14 22:41:26 +0900

    엡라우팅 수정중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen items-center justify-center">
+        <div className='felx flex-col gap-2 p-5 max-w-xs w-full bg-slate-800 rounded-lg'>
+            <div className="text-center my-4">
+                <h1>UNIV3</h1>
+            </div>
+            <div>
+                <Link href = '/signin' className="bg-slate-900 my-4 p-3 rounded-lg block"
+                >Sing In</Link>
+            </div>
+            <div>
+                <Link href = "/signup"
+                className="bg-slate-900 my-4 p-3 rounded-lg block"
+                >Sing Up</Link>
+            </div>
+        </div>
     </main>
   )
 }
