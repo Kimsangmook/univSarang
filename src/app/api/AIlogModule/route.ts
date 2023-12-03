3728051b66900cb2baeb4c830359c76504d49046
Kimsangmook <uj03109@naver.com>
2023-12-03 17:18:28 +0900

    수정

---
+++ src/app/api/AIlogModule/route.ts
@@ -0,0 +1,7 @@
+// import AILogModule from "@/app/api/AIlogModule/AilogModule"
+import { readFileSync } from "fs";
+
+export async function GET(request:Request) {
+    const body = readFileSync('./app/api/AIlogModule/AilogModule')
+    return new Response(body,{headers:{'Content-Type':'text/javascript'}});
+}
